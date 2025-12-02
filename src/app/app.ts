372b2e0480cc70473feaf021cdbc1f48c6e1e6b4
YurikoRasero <yurikorasero@gmail.com>
2025-12-02 10:21:22 +0100

    ultima actualizacion

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { capa2 } from "./capa2/capa2";
+import { capa1 } from "./capa1/capa1";
+import { capa3 } from './capa3/capa3';
+
+
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'componente-inicial',
+  imports: [RouterOutlet, capa2, capa1, capa3],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
