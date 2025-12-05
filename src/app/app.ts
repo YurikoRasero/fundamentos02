cf2d1341adafd9e7537d8fb657572ee82825f4b5
yuri <yurikorasero@gmail.com>
2025-12-05 01:07:07 +0100

    Actulizacion 3.2

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,13 +10,17 @@ import { Interpolacion } from './Binding/interpolacion/interpolacion';
 import { OwbindingA } from './Binding/owbinding-a/owbinding-a';
 import { OwbindingB } from './Binding/owbinding-b/owbinding-b';
 import { Placeholder } from './Binding/placeholder/placeholder';
-
+import { Owbindingc } from './owbinding-c/owbinding-c';
+import { TwbindingCalc } from './twbinding-calc/twbinding-calc';
+import { TwbindingDivisa } from './twbinding-divisa/twbinding-divisa';
 
 
 @Component({
   selector: 'componente-inicial',
   imports: [RouterOutlet,Component1, Component2, Component3, Component4,
-            Component5,Typescrip1, Interpolacion,OwbindingA,OwbindingB,Placeholder],
+            Component5,Typescrip1, Interpolacion,OwbindingA,OwbindingB,
+            Placeholder,Owbindingc,TwbindingCalc,TwbindingDivisa],
+
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/twbinding-divisa/twbinding-divisa.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-twbinding-divisa',
+  imports: [FormsModule],
+  templateUrl: './twbinding-divisa.html',
+  styleUrl: './twbinding-divisa.css',
+})
+
+export class TwbindingDivisa {
+
+ cantidad: number = 0;
+  monedaOrigen: string = 'EUR';
+  monedaDestino: string = 'USD';
+  resultado: number = 0;
+
+  // Tasas ficticias, puedes cambiarlas
+  tasas: any = {
+    EUR: { EUR: 1, USD: 1.1, GBP: 0.85 },
+    USD: { EUR: 0.90, USD: 1, GBP: 0.77 },
+    GBP: { EUR: 1.17, USD: 1.30, GBP: 1 }
+  };
+
+  convertir() {
+    const tasa = this.tasas[this.monedaOrigen][this.monedaDestino];
+    this.resultado = this.cantidad * tasa;
+  }
+
+}
